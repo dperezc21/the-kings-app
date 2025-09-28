eb8a56f65fc88fab34480d34a38013453b610256
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-09-28 17:32:22 -0500

    feat: add new services, cerquillo and adulto con barba

--- constants/service-barber-price.ts
+++ constants/service-barber-price.ts
@@ -5,10 +5,12 @@ export interface BarberPrice {
 }
 
 export const buttons_value: BarberPrice[] = [
-  {service: 'Adultos', price: 2000, image: require('../assets/images/hombre.png')}, 
-  {service: 'Niños', price: 1500, image: require('../assets/images/chico.png')},
-  {service: 'Barba', price: 1000, image: require('../assets/images/bigote-con-barba.png')},
-  {service: 'Cejas', price: 2000, image: require('../assets/images/ceja.png')},
+  {service: 'Adultos', price: 14000, image: require('../assets/images/hombre.png')},
+  {service: 'Adultos con Barba', price: 17000, image: require('../assets/images/barba.png')}, 
+  {service: 'Niños', price: 13000, image: require('../assets/images/chico.png')},
+  {service: 'Barba', price: 7000, image: require('../assets/images/bigote-con-barba.png')},
+  {service: 'Cejas', price: 3000, image: require('../assets/images/ceja.png')},
+  {service: 'Cerquillos', price: 3000, image: require('../assets/images/comercio.png')},
 ];
 
 export type BarberServicePrice = Omit<BarberPrice, 'image'> & {
